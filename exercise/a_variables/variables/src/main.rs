d2bdcc2a82b9805c970021ec396b08e237669354
Ricardo Conceição <ricardo220893@gmail.com>
2024-01-07 14:18:50 -0300

    part 2

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,5 +1,10 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-  let missiles = 8;
-  let ready = 2;
+  let mut missiles = STARTING_MISSILES;
+  let ready = READY_AMOUNT;
+  missiles = missiles - ready;
   println!("Firing {} of my {} missiles...", ready, missiles);
+  println!("{} missiles left", missiles);
 }
