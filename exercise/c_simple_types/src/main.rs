ce52199814653ecda7b063eb3896078dcdc06194
Ricardo Conceição <ricardo220893@gmail.com>
2024-01-07 15:55:21 -0300

    solving exercises 5

--- exercise/c_simple_types/src/main.rs
+++ exercise/c_simple_types/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     // Create an array of type [f32; 2] and initialize it to contain the
     // information from coords.  Uncomment the print_array line and run the code.
     //
-    let coords_arr = [coords.0, coords.1];              // create an array literal out of parts of `coord` here
+    let coords_arr: [f32; 2] = [coords.0, coords.1];              // create an array literal out of parts of `coord` here
     ding_machine::print_array(coords_arr);        // and pass it in here (this line doesn't need to change)
 
 
