66a4b3f91f269891c5d3ba67c2ec9db8b02d2a78
Ricardo Conceição <ricardo220893@gmail.com>
2024-01-07 14:22:30 -0300

    Extra challenges

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,9 +2,8 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-  let mut missiles = STARTING_MISSILES;
-  let ready = READY_AMOUNT;
-  missiles = missiles - ready;
+  let unuser_var: char;
+  let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
   println!("Firing {} of my {} missiles...", ready, missiles);
-  println!("{} missiles left", missiles);
+  println!("{} missiles left", missiles - ready);
 }
